bfdb96bf01aeac8bfa351736058d5f5d7a32b0cc
Abdulmalik Alaql <mloooki.3@gmail.com>
2021-01-18 10:57:20 +0300

    Add DataBase to blog website

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -12,13 +13,44 @@ const posts = [];
 
 const app = express();
 
+
+mongoose.connect("mongodb://localhost:27017/postsDB",{useNewUrlParser: true});
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  body: String
+});
+
+const Post= mongoose.model("Post",postSchema);
+
+// const post = new Post({
+//   title:"Day1",
+//   body: "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla."
+// });
+// post.save();
+
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/",function(req,res){
-  res.render("home",{homeParagraph:homeStartingContent,posts:posts});
+
+  Post.find({},function(err,foundedItems){
+    if(err){
+      console.log(err);
+    } else{
+      console.log("Get DATA Secceefuly");
+    }
+    if(foundedItems ===0){
+      res.render("home",{homeParagraph:homeStartingContent,posts:posts});
+    } else{
+
+      res.render("home",{homeParagraph : homeStartingContent, posts: foundedItems});
+    }
+  })
+  //res.render("home",{homeParagraph:homeStartingContent,posts:posts});
 });
 
 app.get("/about",function(req,res){
@@ -36,41 +68,42 @@ app.get("/compose",function(req,res){
 
 app.post("/compose",function(req,res){
   //console.log(req.body.postBody);
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
     body: req.body.postBody
-  };
+  });
+  post.save();
 
   posts.push(post);
   res.redirect("/");
 
 });
 
-app.get("/posts/:postName",function(req,res){
-   const requestedTitle = _.lowerCase(req.params.postName);
-  var founded = "Not Found!";
-  for(var i = 0 ; i<posts.length;i++){
-    if( _.lowerCase(posts[i].title) === requestedTitle){
-      res.render("post",{title:posts[i].title,paragraph:posts[i].body});
-      break;
-    }else{
-      console.log("Not found");
+app.get("/posts/:postId",function(req,res){
+   const requestedPostId  = req.params.postId;
+  // var founded = "Not Found!";
+  // for(var i = 0 ; i<posts.length;i++){
+  //   if( _.lowerCase(posts[i].title) === requestedTitle){
+  //     res.render("post",{title:posts[i].title,paragraph:posts[i].body});
+  //     break;
+  //   }else{
+  //     console.log("Not found");
+  //   }
+  //
+  // }
+  // console.log(founded);
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    if(err){
+      console.log(err);
+    } else{
+
+      res.render("post",{title:post.title,paragraph:post.body});
     }
 
-  }
-  console.log(founded);
+
+   });
 });
-
-
-
-
-
-
-
-
-
-
-
 
 
 
